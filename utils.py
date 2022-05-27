dfc92a1e57e9a0729cc66df4469a8ab3dccfb115
kvakirsanov <anton.kirsanov@gmail.com>
2022-05-27 12:21:21 +0600

    fix

--- utils.py
+++ utils.py
@@ -10,9 +10,11 @@ def get_time():
 def build_intl_phone_number( number, ton ):
 
     ret = number
+    country_prefix = "996"
     m = re.match(r'^0(\d{3,3})(\d{6,6})$', str(number))
 
     if str(ton) == "161" and m:
-        ret = "996" + m.group(1) + m.group(2)
+        # remove leading '0' and add country prefix
+        ret = country_prefix + m.group(1) + m.group(2)
 
     return ret
